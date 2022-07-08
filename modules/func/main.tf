6334d61a76e8814336a53560ee4b039e14f305ec
levi106 <16788901+levi106@users.noreply.github.com>
2022-07-09 01:00:41 +0900

    initial commit

---
+++ modules/func/main.tf
@@ -0,0 +1,58 @@
+variable "LOCATION" {}
+variable "RESOURCE_GROUP" {}
+variable "STORAGE_ACC_NAME" {}
+variable "STORAGE_ACC_KEY" {}
+variable "STORAGE_CONNECTION_STRING" {}
+
+resource "random_string" "random" {
+  length  = 4
+  special = false
+  lower   = true
+  upper   = false
+}
+
+resource "azurerm_application_insights" "func_application_insights" {
+  name = "func-application-insights"
+  location = var.LOCATION
+  resource_group_name = var.RESOURCE_GROUP
+  application_type = "Node.JS"
+}
+
+resource "azurerm_app_service_plan" "func_app_service_plan" {
+  name = "func-app-service-plan"
+  location = var.LOCATION
+  resource_group_name = var.RESOURCE_GROUP
+  kind = "FunctionApp"
+  reserved = true
+  sku {
+    tier = "Dynamic"
+    size = "Y1"
+  }
+}
+
+resource "azurerm_function_app" "func_function_app" {
+  name = "func-function-app${random_string.random.result}"
+  location = var.LOCATION
+  resource_group_name = var.RESOURCE_GROUP
+  app_service_plan_id = azurerm_app_service_plan.func_app_service_plan.id
+  app_settings = {
+    FUNCTIONS_WORKER_RUNTIME = "node",
+    AzureWebJobsStorage = var.STORAGE_CONNECTION_STRING,
+    APPINSIGHTS_INSTRUMENTATIONKEY = azurerm_application_insights.func_application_insights.instrumentation_key,
+    WEBSITE_RUN_FROM_PACKAGE = "1"
+  }
+  os_type = "linux"
+  storage_account_name = var.STORAGE_ACC_NAME
+  storage_account_access_key = var.STORAGE_ACC_KEY
+  version = "~3"
+  lifecycle {
+    ignore_changes = [
+      app_settings["WEBSITE_RUN_FROM_PACKAGE"]
+    ]
+  }
+  site_config {
+    cors {
+      allowed_origins = ["*"]
+    }
+  }
+}
---
+++ vars.tf
@@ -0,0 +1,6 @@
+variable "RESOURCE_GROUP" {
+  default = "func-rg"
+}
+variable "LOCATION" {
+  default = "JapanEast"
+}
